097257e118e14717502d3cb6023483e6e76e3069
Kim Fasbender <v-kimberlyf@zillowgroup.com>
2019-09-19 18:08:37 -0700

    added functionality for MinHeap and passing all tests

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -8,26 +8,28 @@ class HeapNode
 end
 
 class MinHeap
-
   def initialize
     @store = []
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n) - where n is the number of heap nodes
+  # Space Complexity: O(1) - constant
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    @store << HeapNode.new(key, value)
+    heap_up(@store.length - 1)
   end
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(log n) - where n is the number of heap nodes
+  # Space Complexity: O(1) - constant
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    swap(0, @store.length - 1)
+    removed = @store.pop
+    heap_down(0)
+    return removed.value
   end
-
 
   # Used for Testing
   def to_s
@@ -39,15 +41,15 @@ class MinHeap
     end
 
     output += @store.last.value + "]"
-      
+
     return output
   end
 
   # This method returns true if the heap is empty
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(1) - I assume Ruby is just checking the first index of the array and checking if it's nil or not
+  # Space complexity: O(1) - constant
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.empty?
   end
 
   private
@@ -55,17 +57,37 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(log n) - where n is the number of heap nodes
+  # Space complexity: O(mn) - where m is the space the recursive call is taking up on the stack, and n is the max depth of the tree
   def heap_up(index)
-    
+    parent_index = (index - 1) / 2
+
+    if @store[parent_index].key > @store[index].key && parent_index >= 0
+      swap(parent_index, index)
+      heap_up(parent_index)
+    else
+      return
+    end
   end
 
-  # This helper method takes an index and 
+  # This helper method takes an index and
   #  moves it up the heap if it's smaller
   #  than it's parent node.
+  # Time complexity: O(log n) - where n is the number of heap nodes
+  # Space complexity: O(mn) - where m is the space the recursive call is taking up on the stack, and n is the max depth of the tree
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    left_child_index = index * 2 + 1
+    right_child_index = index * 2 + 2
+
+    if @store[left_child_index].nil?
+      return
+    elsif @store[right_child_index].nil? || @store[left_child_index].key < @store[right_child_index].key
+      swap(index, left_child_index)
+      heap_down(left_child_index)
+    else
+      swap(index, right_child_index)
+      heap_down(right_child_index)
+    end
   end
 
   # If you want a swap method... you're welcome
